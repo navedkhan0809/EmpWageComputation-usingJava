52718eb7a9efd158e6bccc5282d8949cdf35d84e
Naved Khan <navedkhan0809@gmail.com>
2021-04-12 22:54:04 +0530

    EmpWage computation for 20 days UC-5

--- EmpWageComputation/src/empWage/welcome.java
+++ EmpWageComputation/src/empWage/welcome.java
@@ -5,28 +5,32 @@ public class welcome {
 	public static void main(String[] args) {
 
 		// initializing variables
-		int empRatePerHr = 20, salary = 0, empHrs = 0;
+		int empRatePerHr = 20, salary = 0, empHrs = 0, totalSalary = 0, workingdays=20;
 
-		// generating random number
-		double randomCheck = Math.floor(Math.random()*10) % 3;
-		int random = (int)randomCheck;
+		for(int i=1; i<=workingdays; i++) {
+			
+			// generating random number
+			double randomCheck = Math.floor(Math.random()*10) % 3;
+			int random = (int)randomCheck;
 
-		// Applying condition
-		switch (random) {
-		case 1: empHrs = 8;
-		break;
+			// Applying condition
+			switch (random) {
+				case 1: empHrs = 8;
+				break;
 		
-		case 2: empHrs = 4;
-		break;
+				case 2: empHrs = 4;
+				break;
 		
-		default: empHrs = 0;
-		}
+				default: empHrs = 0;
+				}
 
 		// Calculating salary
 		salary = empHrs * empRatePerHr;
-
+		totalSalary = totalSalary + salary; 
+		}
+		
 		// Calculating total salary
-		System.out.println("Total Salary is: "+salary);
+		System.out.println("Total Salary is: "+totalSalary);
 	}
 
 }
