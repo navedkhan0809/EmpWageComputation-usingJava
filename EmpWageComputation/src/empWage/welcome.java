6295950ced371e4c877d41cdcd180868b5b32c7a
Naved Khan <navedkhan0809@gmail.com>
2021-04-12 20:17:16 +0530

    Added part time employee with wage

--- EmpWageComputation/src/empWage/welcome.java
+++ EmpWageComputation/src/empWage/welcome.java
@@ -3,8 +3,29 @@ package empWage;
 public class welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Computation practice problem");
 
+		// initializing variables
+		int empRatePerHr = 20, isParttime = 1, isFulltime = 2, salary = 0, empHrs = 0;
+
+		// generating random number
+		double randomCheck = Math.floor(Math.random()*10) % 3;
+
+		// Applying condition
+		if (randomCheck == isParttime) {
+			empHrs = 8;
+
+		} else if (randomCheck == isFulltime) {
+			empHrs = 4;
+
+		} else {
+			empHrs = 0;
+		}
+
+		// Calculating salary
+		salary = empHrs * empRatePerHr;
+
+		// Calculating total salary
+		System.out.println("Total Salary is: "+salary);
 	}
 
 }
