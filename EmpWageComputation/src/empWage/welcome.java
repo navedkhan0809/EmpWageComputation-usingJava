3a3b8a8abd9d3b2002d599f216aeb11c0844947d
Naved Khan <navedkhan0809@gmail.com>
2021-04-12 22:23:14 +0530

    EmpWage Computation added using case statement

--- EmpWageComputation/src/empWage/welcome.java
+++ EmpWageComputation/src/empWage/welcome.java
@@ -3,8 +3,30 @@ package empWage;
 public class welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Computation practice problem");
 
+		// initializing variables
+		int empRatePerHr = 20, salary = 0, empHrs = 0;
+
+		// generating random number
+		double randomCheck = Math.floor(Math.random()*10) % 3;
+		int random = (int)randomCheck;
+
+		// Applying condition
+		switch (random) {
+		case 1: empHrs = 8;
+		break;
+		
+		case 2: empHrs = 4;
+		break;
+		
+		default: empHrs = 0;
+		}
+
+		// Calculating salary
+		salary = empHrs * empRatePerHr;
+
+		// Calculating total salary
+		System.out.println("Total Salary is: "+salary);
 	}
 
 }
