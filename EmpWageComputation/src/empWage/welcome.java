28b4653cf0e66f887a5a8fa7a72ff5c279f88bc3
Naved Khan <navedkhan0809@gmail.com>
2021-04-12 22:58:41 +0530

    EmpWage computation for 20 days added

--- EmpWageComputation/src/empWage/welcome.java
+++ EmpWageComputation/src/empWage/welcome.java
@@ -3,8 +3,34 @@ package empWage;
 public class welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Computation practice problem");
 
+		// initializing variables
+		int empRatePerHr = 20, salary = 0, empHrs = 0, totalSalary = 0, workingdays=20;
+
+		for(int i=1; i<=workingdays; i++) {
+			
+			// generating random number
+			double randomCheck = Math.floor(Math.random()*10) % 3;
+			int random = (int)randomCheck;
+
+			// Applying condition
+			switch (random) {
+				case 1: empHrs = 8;
+				break;
+		
+				case 2: empHrs = 4;
+				break;
+		
+				default: empHrs = 0;
+				}
+
+		// Calculating salary
+		salary = empHrs * empRatePerHr;
+		totalSalary = totalSalary + salary; 
+		}
+		
+		// Calculating total salary
+		System.out.println("Total Salary is: "+totalSalary);
 	}
 
 }
